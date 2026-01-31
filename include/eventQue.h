284b7bd896eda90172e2dac852cea37b7039aa99
Elmin-Cr <elminc1@hotmail.com>
2026-01-31 20:08:02 +0100

    Added eventqueue, will fix the wrong spelling on queue. Just noticed i have absolutely no comments so will also fix that if i get the time to.

---
+++ include/eventQue.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "event.h"
+
+struct EventQue {
+    Event* Buffer;
+    int capacity;
+    int head;
+    int tail;
+    int count;
+};
+
+EventQue que_create(int capacity);
+void que_destroy(EventQue& q);
+bool que_empty(const EventQue& q);
+bool que_full(const EventQue& q);
+bool que_deque(EventQue& q, Event e);
+bool que_que(EventQue& q, Event& out);
